d1e89e79415b55a2b17a7aa573afafe8666f6432
gerogeali <csd4411@csd.uoc.gr>
2022-04-22 17:33:08 +0300

    Added applications

--- includes/dfunctions.inc.php
+++ includes/dfunctions.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+$current_users = 0;
+$current_applications = 0;
+
 // Connects to DB epignosis if it does exist. Otherwise it gets created, then it connects.
 // Returns the connection with the DB
 function connectDB()
@@ -28,22 +31,61 @@ function initDB($is_connected)
 
     try {
         // add table Users to db
-        $query = "CREATE TABLE users ( accountID INT(30) AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL, password VARCHAR(30) NOT NULL, email VARCHAR(50) NOT NULL, acoount_type INT(30) NOT NULL )";
+        $query = "CREATE TABLE users ( accountID INT(30) PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL, password VARCHAR(30) NOT NULL, email VARCHAR(50) NOT NULL, account_type INT(30) NOT NULL )";
         mysqli_query($is_connected, $query);
-        $query = "INSERT INTO users VALUES ('admin', 'admin', 'admin', 'admin@epignosis.admin.com', 1);";
-        $query .= "INSERT INTO users VALUES ('George', 'Alivertis', '123567', 'George@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Panagiotis', 'Anastasiadis', 'Panagiotis', 'Panagiotis@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Giannis', 'Tzortzakis', 'zsdvdaf', 'Giannis@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Nikos', 'Stathakis', 'x123xaz', 'Nikos@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Tasos', 'Karagianopoulos', 'v25cqW', 'Tasos@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Marios', 'Alivertis' , '2V5# 24 5 ', 'Marios@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Sofia', 'Gounari' ,':-)', 'Sofia@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Maria', 'Gounari' ,'empty', 'Maria@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Eleni', 'Andreou' ,'DROP DATABASE', 'Eleni@epignosis.com', 0);";
-        $query .= "INSERT INTO users VALUES ('Euthimia', 'Panagiotopoulou','admin', 'Euthimia@epignosis.com', 0);";
+        $query = "INSERT INTO users VALUES (0, 'admin', 'admin', 'admin', 'admin@epignosis.admin.com', 1);";
+        $query .= "INSERT INTO users VALUES (1, 'George', 'Alivertis', '123567', 'George@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (2, 'Panagiotis', 'Anastasiadis', 'Panagiotis', 'Panagiotis@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (3, 'Giannis', 'Tzortzakis', 'zsdvdaf', 'Giannis@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (4, 'Nikos', 'Stathakis', 'x123xaz', 'Nikos@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (5, 'Tasos', 'Karagianopoulos', 'v25cqW', 'Tasos@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (6, 'Marios', 'Alivertis' , '2V5# 24 5 ', 'Marios@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (7, 'Sofia', 'Gounari' ,':-)', 'Sofia@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (8, 'Maria', 'Gounari' ,'empty', 'Maria@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (9, 'Eleni', 'Andreou' ,'DROP DATABASE', 'Eleni@epignosis.com', 0);";
+        $query .= "INSERT INTO users VALUES (10, 'Euthimia', 'Panagiotopoulou','admin', 'Euthimia@epignosis.com', 0);";
         mysqli_multi_query($is_connected, $query);
+        $GLOBALS['current_users'] = 11;
     } catch (Exception $e) {
         // If table exists comes here.
+    }
+
+    try {
+        // add table applications to db
+        $query = "CREATE TABLE applications ( accountID INT(30) PRIMARY KEY NOT NULL, submitDay VARCHAR(30) NOT NULL, dateFrom VARCHAR(30) NOT NULL, dateTo VARCHAR(30) NOT NULL, reason VARCHAR(500) NOT NULL, status VARCHAR(30) NOT NULL)";
+        mysqli_query($is_connected, $query);
+        $query = "INSERT INTO applications VALUES (1, '22/04/2022', '05/06/2022', '25/06/2022', 'I want to take a trip to Ohio', 'Pending');";
+        $query .= "INSERT INTO applications VALUES (1, '17/06/2021', '17/07/2021', '27/07/2021', 'I will be moving into a new house and need time to do it', 'Accepted');";
+        $query .= "INSERT INTO applications VALUES (1, '13/12/2020', '24/12/2020', '04/01/2021', 'Christmas vacation', 'Accepted');";
+        $query .= "INSERT INTO applications VALUES (1, '28/09/2020', '29/09/2020', '03/10/2020', 'Need some time of because my mother died', 'Rejected');";
+        $query .= "INSERT INTO applications VALUES (1, '26/07/2020', '06/08/2020', '08/08/2020', 'I need to take some time off for my birthday party', 'Rejected');";
+        $query .= "INSERT INTO applications VALUES (3, '24/07/2020', '27/07/2020', '07/08/2020', 'I want to go to Moscow, need some free time. Thanks!', 'Accepted');";
+        $query .= "INSERT INTO applications VALUES (3, '05/01/2020', '16/01/2020', '17/01/2020', 'I am getting married and will need the day off', 'Accepted');";
+        $query .= "INSERT INTO applications VALUES (2, '01/01/2020', '05/01/2020', '09/01/2020', 'I need extra christmas holidays', 'Rejected');";
+        $query .= "INSERT INTO applications VALUES (2, '20/12/2018', '05/01/2019', '09/01/2019', 'I need extra christmas holidays', 'Accepted');";
+        $query .= "INSERT INTO applications VALUES (2, '05/06/2018', '16/06/2018', '29/08/2018', 'Oops sent the same application twice, sorry!!', 'Rejected');";
+        $query .= "INSERT INTO applications VALUES (2, '05/06/2018', '16/06/2018', '29/08/2018', 'Summer Vacation requested', 'Accepted');";
+        mysqli_multi_query($is_connected, $query);
+        $GLOBALS['current_applications'] = 11;
+    } catch (Exception $e) {
+        // If table exists comes here.
+    }
+}
+
+function getApplications($is_connected, $accountID)
+{
+    return mysqli_query($is_connected, "SELECT * FROM requests where accountID = $accountID ORDER BY submitDay DESC;");
+}
+
+function printApplications($is_connected, $accountID){
+
+    $application = getApplications($is_connected, $accountID);
+
+    while ($temp_application = mysqli_fetch_assoc($application)) {
+        $submitDay = $temp_application['submitDay'];
+        $dateFrom = $temp_application['dateFrom'];
+        $dateTo = $temp_application['dateTo'];
+        $status = $temp_application['status'];
     }
 }
 
@@ -52,7 +94,7 @@ function userLoginCheck($is_connected, $temp_email, $temp_password)
 {
     $stmt = mysqli_stmt_init($is_connected);
 
-    if (!mysqli_stmt_prepare($stmt, "SELECT * FROM users WHERE email = ? and password = ?;")) {
+    if (!mysqli_stmt_prepare($stmt, "SELECT accountID, account_type FROM users WHERE email = ? and password = ?;")) {
         header("location: ../index.php?error=stmtfailed");
         exit();
     }
@@ -63,30 +105,35 @@ function userLoginCheck($is_connected, $temp_email, $temp_password)
 
     $result_set = mysqli_stmt_get_result($stmt);
 
+    $results = mysqli_fetch_assoc($result_set);
+
     if ($result_set == false || mysqli_num_rows($result_set) == 0) {
         return false;
     } else {
         session_start();
-        $_SESSION["type"]; // = (admin || user)
-        $_SESSION["id"];    // to id gia na mporw na kanw tis ektipwseis swsta
+        $_SESSION["type"] = $results['account_type'];
+        $_SESSION["id"] = $results['accountID'];
         return $result_set;
     }
 }
 
 function userSignUpCheck($is_connected, $temp_email)
 {
-    $stmt = mysqli_stmt_init($is_connected);
+    //$stmt = mysqli_stmt_init($is_connected);
 
-    if (!mysqli_stmt_prepare($stmt, "SELECT * FROM users WHERE email = ?;")) {
-        header("location: ../index.php?error=stmtfailed");
-        exit();
-    }
+    //if (!mysqli_stmt_prepare($stmt, "SELECT * FROM users WHERE email = ?;")) {
+    //    header("location: ../index.php?error=stmtfailedsignup$temp_email");
+    //    exit();
+    //}
 
-    mysqli_stmt_bind_param($stmt, "s", $temp_email);
+    $query = "SELECT * FROM users WHERE email = '$temp_email';";
+    $result_set = mysqli_query($is_connected, $query);
 
-    mysqli_stmt_execute($stmt);
+    //mysqli_stmt_bind_param($stmt, "s", $temp_email);
 
-    $result_set = mysqli_stmt_get_result($stmt);
+    //mysqli_stmt_execute($stmt);
+
+    //$result_set = mysqli_stmt_get_result($stmt);
 
     if ($result_set == false || mysqli_num_rows($result_set) == 0) {
         return false;
@@ -99,18 +146,13 @@ function userSignUpCheck($is_connected, $temp_email)
 
 function userInsert($is_connected, $temp_firstName, $temp_last_Name, $temp_email, $temp_password, $temp_accountType)
 {
-    $stmt = mysqli_stmt_init($is_connected);
+    $current_users = intval($GLOBALS['current_users']);
 
-    $query = "INSERT INTO users (firstname, lastname, password, email, account_type) VALUES (?, ?, ?, ?, ?);";
+    $temp_account = intval($temp_accountType);
 
-    if (!mysqli_stmt_prepare($stmt, $query)) {
-        header("location: ../index.php?error=stmtfailed");
-        exit();
-    } else {
-        mysqli_stmt_bind_param($stmt, "ssssi", $temp_firstName, $temp_last_Name, $temp_password, $temp_email, intval($temp_accountType));
-
-        mysqli_stmt_execute($stmt);
-    }
+    $query = "INSERT INTO users VALUES ($current_users, '$temp_firstName', '$temp_last_Name','$temp_password', '$temp_email', $temp_account);";
+    mysqli_query($is_connected, $query);
+    $GLOBALS['current_users'] = $GLOBALS['current_users'] + 1;
 }
 
 // Prints given result set
